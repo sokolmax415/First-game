df061599c3fa744f86a993e5500cfdf73f22e775
temaantonov11 <temaantonov11@yandex.ru>
2024-11-11 16:51:53 +0300

    +health

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int health = 100;
+
+    private int MAX_HEALTH = 100;
+
+    private bool dead = false;
+    
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    public void Damage(int amount)
+    {
+        if (amount < 0) {
+            throw new System.ArgumentOutOfRangeException("Cannot have negative damage");
+        }
+
+        
+        this.health -= amount;
+
+        if (health < 0) {
+            Die();
+        }
+    }
+
+   
+
+    public void Heal(int amount)
+    {
+        if (amount < 0)
+        {
+            throw new System.ArgumentOutOfRangeException("Cannot have negative healing");
+        }
+        bool wouldBeOverMaxHealth = health + amount > MAX_HEALTH;
+
+        if (wouldBeOverMaxHealth)
+        {
+            this.health = MAX_HEALTH;
+        } else
+        {
+            this.health += amount;
+        }
+    }
+
+    private void Die() {
+        Debug.Log("Dead");
+        dead = true;
+    }
+}
